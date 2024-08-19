f6b0141077f2910805d5126dc2e014eed597b019
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-08-19 15:52:41 +0200

    token

--- app/api/auth/[...nextauth]/auth-util.ts
+++ app/api/auth/[...nextauth]/auth-util.ts
@@ -7,53 +7,42 @@ import { retrieveUserServerSide, sanitizeUser } from "../../users/[[...slug]]/se
 
 const VERSION = "0.1.0"
 
-async function authenticateJWTAndGet(username: string, password: string) {
-    let user
-    try {
-        user = await getPrisma().user.findUnique({
-            where: {
-                username: username,
-            },
-            include: {
-                role: {
-                    include: {
-                        permissions: true,
-                    }
+async function authUserWithPassword(username: string, password: string): Promise<IUser | undefined> {
+    return await getPrisma().user.findUnique({
+        where: {
+            username: username,
+        },
+        include: {
+            role: {
+                include: {
+                    permissions: true,
                 }
             }
-        })
-    } catch (ex) {
-        console.log("Login failed:")
-        console.log(ex)
-    }
-
-    if (user != undefined && user.password == password) {
-        return sanitizeUser(user)
-    } else {
-        return undefined
-    }
+        }
+    }).then(user => {
+        if (user != undefined && user.password === password) {
+            return sanitizeUser(user);
+        } else {
+            return undefined;
+        }
+    });
 }
 
 
 const prodivers: any[] = [
     CredentialsProvider({
-        // The name to display on the sign in form (e.g. 'Sign in with...')
         name: 'Credentials',
-        // The credentials is used to generate a suitable form on the sign in page.
-        // You can specify whatever fields you are expecting to be submitted.
-        // e.g. domain, username, password, 2FA token, etc.
-        // You can pass any HTML attribute to the <input> tag through the object.
         credentials: {
             username: { label: "Username", type: "text" },
             password: { label: "Password", type: "password" }
         },
-        async authorize(credentials: any, req: any): Promise<any> {
+        async authorize(credentials, _req): Promise<any> {
             const { username, password } = credentials as {
                 username: string,
                 password: string,
             };
 
-            return await authenticateJWTAndGet(username, password)
+            return await authUserWithPassword(username, password)
         }
     })
 ];
@@ -66,16 +55,14 @@ export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(getPrisma()),
     callbacks: {
         async jwt({ token, user, account, profile, isNewUser }: any) {
-            // Persist the OAuth access_token and or the user id to the token right after signin
-            if (user) {
-                //token.accessToken = account.access_token;
-                token.user = user
+            if (user != undefined) {
+                token.user = user; // Bereits authentifiziert
             } else {
-                const registeredUser: IUser | undefined = await retrieveUserServerSide(token.user?.id)
+                const registeredUser: IUser | undefined = await retrieveUserServerSide(token.user?.id);
                 if (registeredUser == undefined) {
-                    return undefined // user no longer exists
+                    return undefined; // Der Benutzer existiert nicht mehr
                 } else {
-                    token.user = registeredUser
+                    token.user = registeredUser; // Dem Token werden die Informationen angehängt.
                 }
             }
 
@@ -84,10 +71,9 @@ export const authOptions: NextAuthOptions = {
 
         async session({ session, token, user }: any) {
             // Send properties to the client, like an access_token and user id from a provider.
-            //session.accessToken = token.accessToken;
             session.user = token.user
             session.version = VERSION
-            return session
+            return session;
         },
     },
     secret: process.env.NEXTAUTH_SECRET,
